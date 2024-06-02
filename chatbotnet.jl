691e85cc0c7ef09a792be6ee673f5970f68e57b6
ietzel <67884277+ietzel@users.noreply.github.com>
2024-06-02 13:36:13 -0700

    Update chatbotnet.jl

--- chatbotnet.jl
+++ chatbotnet.jl
@@ -1,14 +1,7 @@
-from chatterbot import ChatBot
-
-chatbot = ChatBot("Chatpot")
-
-exit_conditions = (":q", "quit", "exit")
-while True:
-    query = input("> ")
-    if query in exit_conditions:
-        break
-    else:
-        print(f"🪴 {chatbot.get_response(query)}")
+complaint_response = "no is my favorite emotion"
 
 def main():
   print("Chatbotnet App")
+  print("Any complaints?")
+  input()
+  print("complaint_response")
