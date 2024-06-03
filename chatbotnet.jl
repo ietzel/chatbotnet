df471b07aaa484f5c358e7ee4b52cea0e260d5cb
ietzel <67884277+ietzel@users.noreply.github.com>
2024-06-03 15:49:54 -0700

    Update chatbotnet.jl

--- chatbotnet.jl
+++ chatbotnet.jl
@@ -1,10 +1,14 @@
 complaint_response = "no is my favorite emotion"
+problem_response = "try hikes and/or indoor cardio exercise, making/listening to energizing/relaxing music, reading/writing (particularly on a calendar), etc."
 
 function main():
   print("Chatbotnet App")
   print("Any complaints?")
   input()
   print("complaint_response")
+  print("Anything preventing you from doing the things you want?")
+  input()
+  print(problem_response)
 end
 
 main()
