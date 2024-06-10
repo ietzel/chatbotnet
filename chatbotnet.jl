1227e4c2768bff9e3a1afda95fcac005a74a7a59
staccblox <67884277+staccblox@users.noreply.github.com>
2024-06-10 14:51:24 -0700

    Update chatbotnet.jl

--- chatbotnet.jl
+++ chatbotnet.jl
@@ -1,14 +1,17 @@
 complaint_response = "no is my favorite emotion"
 problem_response = "try hikes and/or indoor cardio exercise, making/listening to energizing/relaxing music, reading/writing (particularly on a calendar), etc."
-
+feng_shui_response = "feng shui can be useful for tidying up your career, love, family, wealth, health, transportation, creativity, academics, and fame"
 function main():
   print("Chatbotnet App")
   print("Any complaints?")
   input()
-  print("complaint_response")
+  print(complaint_response)
   print("Anything preventing you from doing the things you want?")
   input()
   print(problem_response)
+  print("Are there disorganization issues?")
+  input()
+  print(feng_shui_response)
 end
 
 main()
